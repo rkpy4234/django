37a2f3b20f80acb60655874f20646fa86c511155
Ram Krishna Yadav <rkpy4234@gmail.com>
2025-01-05 17:14:45 +0545

    input form

---
+++ myProject/myApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import BookForm
+from django.http import HttpResponse
+from .models import Book
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, "myApp/home.html",context)
+
+def book_data(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['bk_name']
+            number = form.cleaned_data['bk_number']
+            book1 = Book.objects.create(bk_name = name, 
+            bk_number = number)
+            book1.save()
+    form = BookForm()
+    return render(request, "myApp/book.html",{'form':form})
